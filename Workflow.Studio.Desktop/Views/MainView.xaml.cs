0a8f726c4dd24e2e7d6ed777063a58f7485f3bdb
JackGuogit <guoexchange@163.com>
2026-05-18 23:23:05 +0800

    创建MainView

---
+++ Workflow.Studio.Desktop/Views/MainView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Workflow.Studio.Desktop.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
